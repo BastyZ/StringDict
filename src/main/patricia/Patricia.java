1be4018bbf51dc36b0da8a60cf46e2d9165f3a6e
BastyZ <bastian.inostroza@gmail.com>
2017-11-03 23:48:44 -0300

    Refactor

---
+++ src/main/patricia/Patricia.java
@@ -0,0 +1,22 @@
+package main.patricia;
+
+import java.util.List;
+import main.StringDictionary;
+
+public class Patricia implements StringDictionary {
+
+  @Override
+  public void insert(String key, int value) {
+
+  }
+
+  @Override
+  public List<Integer> search(String key) {
+    return null;
+  }
+
+  @Override
+  public long getSize() {
+    return 0;
+  }
+}
---
+++ src/main/tst/tst.java
@@ -0,0 +1,22 @@
+package main.tst;
+
+import java.util.List;
+import main.StringDictionary;
+
+public class tst implements StringDictionary {
+
+  @Override
+  public void insert(String key, int value) {
+
+  }
+
+  @Override
+  public List<Integer> search(String key) {
+    return null;
+  }
+
+  @Override
+  public long getSize() {
+    return 0;
+  }
+}
