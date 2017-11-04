c3decede9b0bc1b2893f1af8044268f474f7cbf9
AnnBenavides <andrea.benavidesj@gmail.com>
2017-11-04 17:46:12 -0300

    working on issue #5 & #7 on TST

--- src/main/tst/tst.java
+++ src/main/tst/tst.java
@@ -1,13 +1,26 @@
 package main.tst;
 
+import java.util.ArrayList;
 import java.util.List;
 import main.StringDictionary;
 
 public class tst implements StringDictionary {
+  private ItstNode root;
+  private List<Character> sigma;
 
-  @Override
-  public void insert(String key, int value) {
+  static List<Character> generateSigma (List<String> txt){
+    List<Character> temp = new ArrayList<>();
+    for (String str : txt){
+      for (char chr : str.toCharArray()){
+        temp.add(chr);
+      }
+    }
+    return temp;
+  }
 
+  public tst(List<Character> chars){
+    this.sigma = chars;
+    root = new tstNode('#'); //donde # marca el final de una palabra
   }
 
   @Override
@@ -16,7 +29,21 @@ public class tst implements StringDictionary {
   }
 
   @Override
+  public void insert(String key, int value) {
+
+  }
+
+  @Override
   public long getSize() {
-    return 0;
+    return (sigma.size() + root.getSize());
+  }
+
+  private boolean validString (String str){
+    for (char chr : str.toCharArray()){
+      if(!sigma.contains(chr)){
+        return false;
+      }
+    }
+    return true;
   }
 }
--- src/main/tst/tstNode.java
+++ src/main/tst/tstNode.java
@@ -3,6 +3,16 @@ package main.tst;
 import java.util.List;
 
 public class tstNode implements ItstNode{
+  private ItstNode father;
+  private ItstNode left;
+  private ItstNode middle;
+  private ItstNode right;
+  private List<Integer> value;
+  private char data;
+
+  public tstNode(char data){
+
+  }
 
   @Override
   public boolean isNull() {
@@ -11,57 +21,58 @@ public class tstNode implements ItstNode{
 
   @Override
   public void setValue(int value) {
-
+    this.value.add(value);
   }
 
   @Override
   public void setLeft(ItstNode node) {
-
+    this.left = node;
   }
 
   @Override
   public void setMiddle(ItstNode node) {
-
+    this.middle = node;
   }
 
   @Override
   public void setRight(ItstNode node) {
-
+    this.right = node;
   }
 
   @Override
   public ItstNode insert(String key, int value, int index) {
+    /*TODO*/
     return null;
   }
 
   @Override
   public List<Integer> getValue() {
-    return null;
+    return this.value;
   }
 
   @Override
   public char getData() {
-    return 0;
+    return this.data;
   }
 
   @Override
   public ItstNode getFather() {
-    return null;
+    return this.father;
   }
 
   @Override
   public ItstNode getLeftChild() {
-    return null;
+    return this.left;
   }
 
   @Override
   public ItstNode getMiddleChild() {
-    return null;
+    return this.middle;
   }
 
   @Override
   public ItstNode getRightChild() {
-    return null;
+    return this.right;
   }
 
   @Override
