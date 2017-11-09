c9d870a95a12804519fd89780e832d384f309037
BastyZ <bastian.inostroza@gmail.com>
2017-11-09 16:43:22 -0300

    Completada la primera parte del experimento, relacionada a:
    - Construcción de Árboles
    - Busqueda de elementos
    - Busqueda de elementos fuera del texto
    
    Además se agrega la carpeta /text con tres libros c:

---
+++ src/main/Main.java
@@ -0,0 +1,151 @@
+package main;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+import main.hlp.hashTableLP;
+import main.patricia.Patricia;
+import main.tst.tst;
+
+public class Main {
+  private final static String alfabeta = "abcdefghijklmnopqrstuvwxyz ";
+  /* La idea es que esta clase encuentre todos los textos de la carpeta
+  ./files/ y para cada uno, secuencialmente:
+
+  - Cree los tres diccionarios, estimando el tamaño y calculando el tiempo de ejecución
+  - Realice los experimentos en orden
+
+  Idealmente deberían guardarse los resultados en un archivo tipo CSV para facilitar el informe
+   */
+
+  public static void main(String[] args) throws IOException {
+    ArrayList<Path> books = new ArrayList<Path>();
+    final File assetsLocation = new File("/text");
+    for (final File book : assetsLocation.listFiles() ){
+      books.add(book.toPath());
+    }
+    for (final Path book: books) {
+      runExperimentOne(book);
+    }
+
+  }
+
+  private static void runExperimentOne(Path book) throws IOException {
+    ArrayList<Double> times = new ArrayList<>();
+    List<String> words = null;
+    Scanner sc = new Scanner(book);
+    while (sc.hasNextLine()) {
+      String line = sc.nextLine(); // debería ser solo una
+      String[] line_words = line.split(" ");
+      for (String word : line_words) {
+        words.add(word);
+      }
+    }
+    List<Character> chars = null;
+    for (char a : alfabeta.toCharArray()) {
+      chars.add(a);
+    }
+    // ya tenemos todas las palabras en orden en words
+    System.out.println("----------------------------------------------------------");
+    System.out.println("----------------------------------------------------------");
+    System.out.println("El libro "+book.getFileName());
+    System.out.println("Posee "+words.size()+" palabras");
+    StringDictionary patricia = new Patricia();
+    StringDictionary hlp = new hashTableLP(words.size());
+    StringDictionary tst = new tst(chars);
+    Stopwatch stopwatch = new Stopwatch();
+    makeDictionary(patricia,words);
+    times.add(stopwatch.elapsedTime());
+    stopwatch.reset();
+    makeDictionary(tst,words);
+    times.add(stopwatch.elapsedTime());
+    stopwatch.reset();
+    makeDictionary(hlp,words);
+    times.add(stopwatch.elapsedTime());
+    System.out.println("- - - -- - - - -- - - - - - - -- - - - - - - - - - - - - -");
+    System.out.println("Tiempo de Construcción   EXPERIMENTO 1");
+    System.out.println("Patricia:     "+times.get(0));
+    System.out.println("TST:          "+times.get(1));
+    System.out.println("HLP:          "+times.get(2));
+    System.out.println("-----------------------------------------------------------");
+    List<String> randomWords = null;
+    List<String> missingWords = null;
+    LoremIpsum lore = new LoremIpsum();
+    Random number = new Random();
+    for (int i = 0; i < words.size()/10; i++) {
+      randomWords.add(words.get(number.nextInt(words.size())));
+    }
+    String[] miss = lore.getWords(49).split(" ");
+    for (int j = 0; j < 50; j++) {
+      missingWords.add(miss[j]);
+    }
+    System.out.println("Tiempo de busqueda para "+randomWords.size()+" palabras");
+    stopwatch.reset();
+    for (int i = 0; i < randomWords.size(); i++) {
+      patricia.search(randomWords.get(i));
+    }
+    times.add(stopwatch.elapsedTime()); // 3
+    stopwatch.reset();
+    for (int i = 0; i < randomWords.size(); i++) {
+      tst.search(randomWords.get(i));
+    }
+    times.add(stopwatch.elapsedTime()); // 4
+    stopwatch.reset();
+    for (int i = 0; i < randomWords.size(); i++) {
+      hlp.search(randomWords.get(i));
+    }
+    times.add(stopwatch.elapsedTime()); // 5
+    stopwatch.reset();
+    int mRandom = gal(randomWords);
+    System.out.println("Para un m de "+mRandom);
+    System.out.println("Patricia:   "+times.get(3));
+    System.out.println("TST:          "+times.get(4));
+    System.out.println("HLP:          "+times.get(5));
+    System.out.println("-----------------------------------------------------------");
+    System.out.println("Tiempo de fallo para "+missingWords.size()+" palabras");
+    System.out.println("Para un m de "+gal(missingWords));
+    stopwatch.reset();
+    for (int i = 0; i < missingWords.size(); i++) {
+      patricia.search(missingWords.get(i));
+    }
+    times.add(stopwatch.elapsedTime()); // 6
+    stopwatch.reset();
+    for (int i = 0; i < missingWords.size(); i++) {
+      tst.search(missingWords.get(i));
+    }
+    times.add(stopwatch.elapsedTime()); // 7
+    stopwatch.reset();
+    for (int i = 0; i < missingWords.size(); i++) {
+      hlp.search(missingWords.get(i));
+    }
+    times.add(stopwatch.elapsedTime()); // 8
+    stopwatch.reset();
+    System.out.println("Patricia:   "+times.get(6));
+    System.out.println("TST:          "+times.get(7));
+    System.out.println("HLP:          "+times.get(8));
+    System.out.println("-----------------------------------------------------------");
+
+  }
+
+  private static int gal(List<String> words) {
+    int m = 0;
+    for (int i = 0; i < words.size(); i++) {
+      m += words.get(i).length();
+    }
+    return m/words.size();
+
+  }
+
+  private static void makeDictionary(StringDictionary dictionary, List<String> words) {
+    int value = 0;
+    for (String word : words) {
+      dictionary.insert(word+"$",value);
+      value++;
+    }
+  }
+
+}
